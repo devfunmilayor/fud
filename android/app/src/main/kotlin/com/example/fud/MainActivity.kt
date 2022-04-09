b2c9323077ee142f6dd654606a59acbc4b746047
devfunmilayor <96455442+devfunmilayor@users.noreply.github.com>
2022-04-09 16:33:50 +0100

    checks

---
+++ android/app/src/main/kotlin/com/example/fud/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.fud
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/util/constant.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class App {
+  static const String apiUrl =
+      'https://storage.googleapis.com/ember-deploy.appspot.com/images/open-source.json';
+  static const Color primaryColor = Color(0xffFFC703);
+  static const secondaryColor = Color(0xff003A4D);
+  static const barTextColor = Color(0xff9CA6BA);
+}
